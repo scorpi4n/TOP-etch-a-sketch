0b162081cbe7b945c98c14fb374376dcd7cf69ff
James Phillips <scorpi4n@scorpi4n.com>
2022-06-02 18:56:04 -0500

    Update gui and logic

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function drawSketchboard(size = 16) {
     for (let i = 0; i < size**2; i++) {
         const cell = document.createElement('div')
         cell.classList.add('cell')
-        cell.innerText = i
+        // cell.innerText = i
         sketchboard.appendChild(cell)
     }
 }
