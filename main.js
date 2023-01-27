003da8d872aa4b68ec821151da53093baf11017b
James Phillips <scorpi4n@scorpi4n.com>
2023-01-27 13:56:02 -0600

    Refactor eventListeners and add validation

--- main.js
+++ main.js
@@ -1,51 +1,57 @@
-// html elements
-const sketchboard = document.querySelector('.sketchboard')
-const colorPicker = document.getElementById('colorPicker')
-const sizeInput = document.getElementById('sizeInput')
-const eraserBtn = document.getElementById('eraserBtn')
-const resetBtn = document.getElementById('resetBtn')
-const submitBtn = document.getElementById('submitBtn')
+const sketchboard = document.querySelector(".sketchboard");
+const colorPicker = document.getElementById("colorPicker");
+const sizeInput = document.getElementById("sizeInput");
+const eraserBtn = document.getElementById("eraserBtn");
+const resetBtn = document.getElementById("resetBtn");
+const submitBtn = document.getElementById("submitBtn");
 
-let background = '#000'
-colorPicker.addEventListener('change', () => {
-    background = colorPicker.value
-})
+let mousedown = false;
+sketchboard.onmousedown = () => (mousedown = true);
+sketchboard.onmouseup = () => (mousedown = false);
+drawSketchboard();
+
+colorPicker.value = "#000";
+let background = colorPicker.value;
+colorPicker.addEventListener("change", () => {
+	background = colorPicker.value;
+});
 
 function drawSketchboard(size = 16) {
-    if (size > 100 || size < 2) {
-        alert('Please submit a number 2-100')
-    } else {
-        sketchboard.style.display = 'grid'
-        sketchboard.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-        sketchboard.style.gridTemplateRows = `repeat(${size}, 1fr)`
+	if (size < 2 || size > 100) {
+		alert("Please submit a number 2-100");
+		drawSketchboard();
+		return;
+	}
+	sketchboard.style.display = "grid";
+	sketchboard.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	sketchboard.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-        for (let i = 0; i < size**2; i++) {
-            const cell = document.createElement('div')
-            cell.classList.add('cell')
-            cell.style.backgroundColor = 'white'
-            sketchboard.appendChild(cell)
-            cell.addEventListener('mouseenter', () => {
-                cell.style.backgroundColor = background
-            })
-        }        
-    }
-}
-drawSketchboard()
-
-function reset(size = 16) {
-
-    sizeInput.value = 16
-    colorPicker.value = "#000"
-    drawSketchboard(16)
+	for (let i = 0; i < size ** 2; i++) {
+		const cell = document.createElement("div");
+		cell.classList.add("cell");
+		cell.style.backgroundColor = "white";
+		sketchboard.appendChild(cell);
+		cell.addEventListener("mouseover", () => {
+			if (mousedown) cell.style.backgroundColor = background;
+		});
+		cell.addEventListener("click", () => {
+			cell.style.backgroundColor = background;
+		});
+	}
 }
 
-eraserBtn.addEventListener('click', () => {
-    background = '#FFF'
-})
-resetBtn.addEventListener('click', reset)
-submitBtn.addEventListener('click', () => {
-    while (sketchboard.lastChild) {
-        sketchboard.removeChild(sketchboard.lastChild)
-    }
-    drawSketchboard(sizeInput.value)
-})
+function reset() {
+	sizeInput.value = 16;
+	colorPicker.value = "#000";
+	sketchboard.innerHTML = "";
+	drawSketchboard(16);
+}
+
+eraserBtn.addEventListener("click", () => {
+	background = "#FFF";
+});
+resetBtn.addEventListener("click", reset);
+submitBtn.addEventListener("click", () => {
+	sketchboard.innerHTML = "";
+	drawSketchboard(sizeInput.value);
+});
